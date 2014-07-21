300775a151427b5c02f8a0d2cdbec17e321c2386
Martin Wtorkowski <mwtorkowski@getzendoc.com>
2014-07-21 14:36:22 +0200

    Tri state option for checkboxes. Default size set to reflect default checkbox as closely as possible.

--- angular-bootstrap-checkbox.js
+++ angular-bootstrap-checkbox.js
@@ -1,76 +1,89 @@
 "use strict";
 
 angular.module("ui.checkbox", []).directive("checkbox", function() {
-	return {
-		scope: {},
-		require: "ngModel",
-		restrict: "E",
-		replace: "true",
-		template: "<button type=\"button\" ng-style=\"stylebtn\" class=\"btn btn-default\" ng-class=\"{'btn-xs': size==='default', 'btn-sm': size==='large', 'btn-lg': size==='largest'}\">" +
-			"<span ng-style=\"styleicon\" class=\"glyphicon\" ng-class=\"{'glyphicon-ok': checked===true}\"></span>" +
-			"</button>",
-		link: function(scope, elem, attrs, modelCtrl) {
-			scope.size = "default";
-			// Default Button Styling
-			scope.stylebtn = {};
-			// Default Checkmark Styling
-			scope.styleicon = {"width": "10px", "left": "-1px"};
-			// If size is undefined, Checkbox has normal size (Bootstrap 'xs')
-			if(attrs.large !== undefined) {
-				scope.size = "large";
-				scope.stylebtn = {"padding-top": "2px", "padding-bottom": "2px", "height": "30px"};
-				scope.styleicon = {"width": "8px", "left": "-5px", "font-size": "17px"};
-			}
-			if(attrs.larger !== undefined) {
-				scope.size = "larger";
-				scope.stylebtn = {"padding-top": "2px", "padding-bottom": "2px", "height": "34px"};
-				scope.styleicon = {"width": "8px", "left": "-8px", "font-size": "22px"};
-			}
-			if(attrs.largest !== undefined) {
-				scope.size = "largest";
-				scope.stylebtn = {"padding-top": "2px", "padding-bottom": "2px", "height": "45px"};
-				scope.styleicon = {"width": "11px", "left": "-11px", "font-size": "30px"};
-			}
+  return {
+    scope: {
+      triState: '='
+    },
+    require: "ngModel",
+    restrict: "E",
+    replace: "true",
+    template: "<button type=\"button\" ng-style=\"stylebtn\" class=\"btn btn-default\" ng-class=\"{'btn-xxs btn-checkbox': size==='default', 'btn-sm': size==='large', 'btn-lg': size==='largest'}\">" +
+      "<span ng-style=\"styleicon\" class=\"glyphicon\" ng-class=\"{'glyphicon-ok': checked===true, 'glyphicon-minus': checked===0}\"></span>" +
+      "</button>",
+    link: function(scope, elem, attrs, modelCtrl) {
+      // Default state
+      scope.checked = false;
 
-			var trueValue = true;
-			var falseValue = false;
+      scope.size = "default";
+      // Default Button Styling
+      scope.stylebtn = {};
+      // Default Checkmark Styling
+      scope.styleicon = {"width": "10px", "left": "-1px"};
+      // If size is undefined, Checkbox has normal size (Bootstrap 'xs')
+      if(attrs.large !== undefined) {
+        scope.size = "large";
+        scope.stylebtn = {"padding-top": "2px", "padding-bottom": "2px", "height": "30px"};
+        scope.styleicon = {"width": "8px", "left": "-5px", "font-size": "17px"};
+      }
+      if(attrs.larger !== undefined) {
+        scope.size = "larger";
+        scope.stylebtn = {"padding-top": "2px", "padding-bottom": "2px", "height": "34px"};
+        scope.styleicon = {"width": "8px", "left": "-8px", "font-size": "22px"};
+      }
+      if(attrs.largest !== undefined) {
+        scope.size = "largest";
+        scope.stylebtn = {"padding-top": "2px", "padding-bottom": "2px", "height": "45px"};
+        scope.styleicon = {"width": "11px", "left": "-11px", "font-size": "30px"};
+      }
 
-			// If defined set true value
-			if(attrs.ngTrueValue !== undefined) {
-				trueValue = attrs.ngTrueValue;
-			}
-			// If defined set false value
-			if(attrs.ngFalseValue !== undefined) {
-				falseValue = attrs.ngFalseValue;
-			}
+      var trueValue = true;
+      var falseValue = false;
+      var triState = {'false': 0, '0': true, 'true': false};
 
-			// Check if name attribute is set and if so add it to the DOM element
-			if(scope.name !== undefined) {
-				elem.name = scope.name;
-			}
+      // If defined set true value
+      if(attrs.ngTrueValue !== undefined) {
+        trueValue = attrs.ngTrueValue;
+      }
+      // If defined set false value
+      if(attrs.ngFalseValue !== undefined) {
+        falseValue = attrs.ngFalseValue;
+      }
 
-			// Update element when model changes
-			scope.$watch(function() {
-				if(modelCtrl.$modelValue === trueValue || modelCtrl.$modelValue === true) {
-					modelCtrl.$setViewValue(trueValue);
-				} else {
-					modelCtrl.$setViewValue(falseValue);
-				}
-				return modelCtrl.$modelValue;
-			}, function(newVal, oldVal) {
-				scope.checked = modelCtrl.$modelValue === trueValue;
-			}, true);
+      // Check if name attribute is set and if so add it to the DOM element
+      if(scope.name !== undefined) {
+        elem.name = scope.name;
+      }
 
-			// On click swap value and trigger onChange function
-			elem.bind("click", function() {
-				scope.$apply(function() {
-					if(modelCtrl.$modelValue === falseValue) {
-						modelCtrl.$setViewValue(trueValue);
-					} else {
-						modelCtrl.$setViewValue(falseValue);
-					}
-				});
-			});
-		}
-	};
+      // Update element when model changes
+      scope.$watch(function() {
+        if (!scope.triState) {
+          if(modelCtrl.$modelValue === trueValue || modelCtrl.$modelValue === true) {
+            modelCtrl.$setViewValue(trueValue);
+          } else {
+            modelCtrl.$setViewValue(falseValue);
+          }
+        }
+        return modelCtrl.$modelValue;
+      }, function() {
+        //scope.checked = modelCtrl.$modelValue === trueValue;
+        scope.checked = modelCtrl.$modelValue;
+      }, true);
+
+      // On click swap value and trigger onChange function
+      elem.bind("click", function() {
+        scope.$apply(function() {
+          if (scope.triState) {
+            modelCtrl.$setViewValue(triState[''+modelCtrl.$modelValue]);
+          } else {
+            if(modelCtrl.$modelValue === falseValue) {
+              modelCtrl.$setViewValue(trueValue);
+            } else {
+              modelCtrl.$setViewValue(falseValue);
+            }
+          }
+        });
+      });
+    }
+  };
 });
